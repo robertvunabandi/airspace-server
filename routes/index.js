3b2676870fda55c4af6ee780e96161f5a58cc830
Robert Vunabandi <rvunabandi@gmail.com>
2017-07-11 16:07:43 -0700

    modified index for tests

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
+//entire link: https://mysterious-headland-54722.herokuapp.com/
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
-});
-
-/* GET home page. */
-router.get('/test', function(req, res, next) {
-	res.send("Hello");
+var testResponse = {
+	numbers: [0,1,2,3,4,5,6],
+	names: ["Ruben", "Amanda", "Robert"]
+};
+/* Test page */
+router.get('/test', function(request, response, next) {
+	response.setHeader('Content-Type', 'application/json');
+	response.send(JSON.stringify(testResponse), null, 3);
 });
 
 module.exports = router;
+
+/*
+
+Heroku pushes:
+
+git add <files>
+git commit -m "messages"
+git push origin master
+
+git push heroku master
+
+*/
