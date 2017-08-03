f6ffa01b217942466ee5b0a090dd8665288128b7
Robert Vunabandi <rvunabandi@gmail.com>
2017-08-03 15:09:24 -0700

    make images work by making placeholder urls be NULL

--- img_hlps.js
+++ img_hlps.js
@@ -1,8 +1,8 @@
 let helpers = require('./helpers');
 
 let img_hlps = {
-	PROFILE_IMAGE_PLACEHOLDER_URL: "",
-	REQUEST_IMAGE_PLACEHOLDER_URL: ""
+	PROFILE_IMAGE_PLACEHOLDER_URL: "NULL",
+	REQUEST_IMAGE_PLACEHOLDER_URL: "NULL"
 };
 
 module.exports = img_hlps;
